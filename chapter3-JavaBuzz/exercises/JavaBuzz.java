2f0fbaa21d24ac7b1f6ae1181cb310178770e693
Simone <simonecaplin@gmail.com>
2018-08-08 11:18:35 +0100

    JavaBuzz

--- chapter3-JavaBuzz/exercises/JavaBuzz.java
+++ chapter3-JavaBuzz/exercises/JavaBuzz.java
@@ -1,5 +1,19 @@
 // can you remember how to set up your main function?
-
-// "Java" if divisible by 3
-// "Buzz" if divisible by 5
-// "JavaBuzz" if divisible by 15
+public class JavaBuzz {
+  public static void main(String[] args) {
+    for(int i=1; i<101; i++){
+      // "JavaBuzz" if divisible by 15
+      if(i % 5 == 0 && i % 3 == 0){
+        System.out.println("JavaBuzz");
+      // "Java" if divisible by 3
+      }else if(i % 3 == 0){
+        System.out.println("Java");
+      // "Buzz" if divisible by 5
+      }else if(i % 5 == 0){
+        System.out.println("Buzz");
+      }else{
+        System.out.println(i);
+      }
+    }
+  }
+}
