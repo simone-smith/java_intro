afc2bbaeeb3d8893743363480636d2c779db2512
Simone <simonecaplin@gmail.com>
2018-08-08 10:48:11 +0100

    Control flow bonus
    * Use while loop to continue checking until user types 17

--- chapter3-JavaBuzz/exercises/ControlFlow.java
+++ chapter3-JavaBuzz/exercises/ControlFlow.java
@@ -3,17 +3,21 @@ import java.util.Scanner;
 // can you remember how to set up your main function?
 public class ControlFlow {
   public static void main(String[] args) {
-    System.out.println("Enter an integer: ");
-    // find a way to read in user input
-    Scanner scanner = new Scanner(System.in);
-    int integer = scanner.nextInt();
+    //  bonus: use a while loop to continue checking until the user types 17
+    int integer = 0;
+    while(integer != 17){
+      System.out.println("Enter an integer: ");
+      // find a way to read in user input
+      Scanner scanner = new Scanner(System.in);
+      integer = scanner.nextInt();
     // write a method that will check if a number is odd or even
     // (assume user only ever enters integers)
     // print the answer to the console
-    if((integer % 2) == 0) {
-      System.out.println("The integer you entered is even");
-    }else{
-      System.out.println("The integer you entered is odd");
+      if((integer % 2) == 0) {
+        System.out.println(integer + " is even");
+      }else{
+        System.out.println(integer + " is odd");
+      }
     }
   }
 }
